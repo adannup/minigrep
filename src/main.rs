7759a661cb16d8a4fc90b09b35ad4446ec48c28b
Ramon Adan <adannup@Ramons-MacBook-Pro-16.local>
2024-03-20 20:54:05 -0600

    change standard output with standard error

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         std::process::exit(1); // exit with error code 1
     });
 
-    println!("{:?}", args);
-
-    println!("In file {}", config.file_path);
-    println!("Searching for {}", config.query);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         std::process::exit(1);
     }
 
